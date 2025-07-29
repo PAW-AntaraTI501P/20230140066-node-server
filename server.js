f0a9764199ef931fa973e62c926a384844c94a92
halimbagusalhakim <halsat132@gmail.com>
2025-07-29 09:49:09 +0700

    update routing

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const port = 3001;
-app.use(cors());
+app.use(express.json());
+
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Node.js Server!" });
+  res.render("index");//renderi file index.ejs
 });
 
+app.get("/contact", (req, res) => {
+  res.render("contact"); //render file contact.ejs
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404 - page not found"); //render file 404.ejs for not found
+});
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
