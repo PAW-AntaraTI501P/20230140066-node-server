9ae7089b234362cadd8095ff832b9ceaea0a72d4
halimbagusalhakim <halsat132@gmail.com>
2025-08-05 09:51:22 +0700

    praktikum rest API postman

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+
+const todoRoutes = require("./routes/todo.js");
+const { todos } = require("./routes/todo.js");
+
 app.use(express.json());
+app.use("/todos", todoRoutes); // Use the todo routes
 
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
