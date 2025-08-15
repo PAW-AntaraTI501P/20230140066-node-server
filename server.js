bc87d91d4acf70804508e980557110b613e77d48
halimbagusalhakim <halsat132@gmail.com>
2025-08-15 10:23:49 +0700

    template views

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const todoRoutes =require("./routes/tododb.js");
 const { todos } = require("./routes/todo.js");
 const db = require("./database/db");
 
+
+
+const expressLayouts = require("express-ejs-layouts");
+app.use(expressLayouts);
 app.use(cors());
 app.use(express.json());
 app.set("view engine", "ejs");
@@ -18,28 +22,35 @@ app.get("/todos-data", (req, res) => {
 });
 
 app.get("/todos-list", (req, res) => {
-  res.render("todos-page", { todos: todos });
+  res.render("todos-page", { todos: todos, layout: "layouts/main-layout" });
 });
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    layout: "layouts/main-layout",
+  });
+    
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact");
+  res.render("contact", {
+    layout: "layouts/main-layout" 
+  });
 });
 
 app.get("/todo-view", (req, res) => {
   db.query("SELECT * FROM todos", (err, todos) => {
     if (err) {
+      console.error("Error fetching todos:", err);
       return res.status(500).send("Internal Server Error");
     }
-    res.render("todo", { todos: todos });
+    res.render("todo", { todos: todos, layout: "layouts/main-layout" });
   });
 });
 
 app.use((req, res) => {
   res.status(404).send("404 - Page Not Found");
+  
 });
 
 app.listen(port, () => {
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,4 +1,4 @@
 let todos = [
 //   { id: 1, task: "Mengerjakan PR" },
 //   { id: 2, task: "Membeli bahan makanan" },
-// ];
+];
